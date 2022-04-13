52d1368e66213a9712a05876b989f5da3eb8f66f
German Trigo Cortes <contacto@alphacorp.cl>
2022-04-12 23:45:36 -0400

    Prueba V4 - Function Toggle

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,3 +3,51 @@ $(document).ready(function(){
         alert("El mensaje fue enviado correctamente!");
     });
 });
+
+function toggle1() {
+
+    var x = document.getElementById('tt1');
+
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    }
+    else {
+      x.style.display = "none";
+    }
+}
+
+function toggle2() {
+
+    var x = document.getElementById('tt2');
+
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    }
+    else {
+      x.style.display = "none";
+    }
+}
+
+function toggle3() {
+
+    var x = document.getElementById('tt3');
+
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    }
+    else {
+      x.style.display = "none";
+    }
+}
+
+function toggle4() {
+
+    var x = document.getElementById('tt4');
+
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    }
+    else {
+      x.style.display = "none";
+    }
+}
